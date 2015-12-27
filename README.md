bf66b53ec35b95120769a561c93ae5be447ffc29
BruceDev <noxusblade@hotmail.com>
2015-12-27 16:24:27 -0500

    Added doc blocks

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ If you do not have a composer.json file already then create one, and put the fol
 
     {
         "require": {
-            "PlasmaCode/TwitchAPI",
+            "PlasmaCode/Twitch",
         }
     }
 
 
 Create an instance of this TwitchAPI with the following code:
 
-    use PlasmaCode\TwitchAPI;
+    use PlasmaCode\Twitch;
     
-    $Twitch = new TwitchAPI;
+    $Twitch = new Api;
--- src/Api.php
+++ src/Api.php
@@ -2,22 +2,36 @@
 namespace PlasmaCode\Twitch;
 
 
-use PlasmaCode\Twitch\Configurations;
-
 class Api
 {
     public $clientID;
     private $clientSecret;
     public $client;
     
+    /** @constant string Contains the base URL for Guzzle requests. */
     const BASEURI = 'https://api.twitch.tv/kraken/';
     
+    /**
+     * Sets up a Guzzle Client for easy request calls.
+     * 
+     * @return void;
+     */
     public function __construct()
     {    
         $this->client = new \GuzzleHttp\Client(['base_uri' => $this::BASEURI, 'verify' => 'C:\programming\php-development\bin\ca-bundle.crt']);
     }
     
-    
+    /**
+     * Grabs simple information from a specified channel.
+     * 
+     * This method is used to specify the desired channel and the type of information it wishes to grab.
+     * I have setup some shortcuts like "id" in place of "_id".
+     * 
+     * @param string $channel
+     * @param string $type
+     * @return string|null|boolean
+     * @throws Exception
+     */
     public function channel($channel, $type)
     {
         $channelReq = $this->client->request('GET', 'channels/' . $channel);
@@ -40,6 +54,15 @@ class Api
         return $channel->$type;
     }
     
+    /**
+     * Decodes a json response.
+     * 
+     * May add different decoding options in the future.
+     * 
+     * @param string $response
+     * @return string
+     * @throws Exception
+     */
     private function decodeResponse($response)
     {
         $decoded = json_decode($response);
